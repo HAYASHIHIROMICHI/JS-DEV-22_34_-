3d4ed8c7f4a5862f0f12ecff595582fc03b34f8a
HAYASHIHIROMICHI <rinrin08280828@gmail.com>
2021-12-11 08:11:25 +0900

    Add files via upload

---
+++ 課題提出2/README.md
@@ -0,0 +1,3 @@
+課題1 JS　ジャンケンアプリ
+参考サイト：https://janken.asotetu.work/js_jaken/
+　　　　　　基礎的な振り返りと記載方法について
---
+++ 課題提出2/js/janken.js
@@ -0,0 +1,62 @@
+
+// /ここからスタート/ 
+
+// /HIKAKIN ジャンケン/ 
+$("#gu_pic").on("click", function () {
+    $("#me").html('<img src="./img/gu.png">');
+  });
+  
+  $("#cho_pic").on("click", function () {
+    $("#me").html('<img src="./img/choki.png">');
+  });
+
+  $("#par_pic").on("click", function () {
+    $("#me").html('<img src="./img/pah.png">');
+  });
+  
+  $("#gu_pic").on("click", function () {
+    var humanGu = 1;
+    var comp = Math.floor(Math.random() * 3 + 1);
+  
+  if (comp == 1) {
+     $("#pc").html('<img src="./img/gu.png">');
+     $("#result").text("あいこです");
+    } else if (comp == 2) {
+      $("#pc").html('<img src="./img/choki.png">');
+      $("#result").text("あなたの勝ちです");
+    } else if (comp == 3) {
+      $("#pc").html('<img src="./img/pah.png">');
+      $("#result").text("ヒカキンの勝ちです");
+    }
+  });
+  
+  $("#cho_pic").on("click", function () {
+    var humaCho = 2;
+    var comp = Math.floor(Math.random() * 3 + 1);
+  
+    if (comp == 1) {
+      $("#pc").html('<img src="./img/gu.png">');
+      $("#result").text("ヒカキンの勝ちです");
+    } else if (comp == 2) {
+      $("#pc").html('<img src="./img/choki.png">');
+      $("#result").text("あいこです");
+    } else if (comp == 3) {
+      $("#pc").html('<img src="./img/pah.png">');
+      $("#result").text("あなたの勝ちです");
+    }
+  });
+  
+  $("#par_pic").on("click", function () {
+    var humaPar = 3;
+    var comp = Math.floor(Math.random() * 3 + 1);
+    if (comp == 1) {
+      $("#pc").html('<img src="./img/gu.png">');
+      $("#result").text("あなたの勝ちです");
+    } else if (comp == 2) {
+      $("#pc").html('<img src="./img/choki.png">');
+      $("#result").text("ヒカキンの勝ちです");
+    } else if (comp == 3) {
+      $("#pc").html('<img src="./img/pah.png">');
+      $("#result").text("あいこです");
+    }
+  });
